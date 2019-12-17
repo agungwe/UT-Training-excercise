730393a0b7666b87195f9a18fb7930ce67734132
Agung Wicaksono <agungw@ecampus.ut.ac.id>
2019-12-17 10:10:54 +0700

    Add profile.go

---
+++ Profile/profile.go
@@ -0,0 +1,25 @@
+package profile
+
+import "fmt"
+
+type User struct {
+	Firstname string
+	Lastname  string
+	Address   string
+}
+
+func (u User) GetProfile() {
+	u.Firstname = "Agung"
+	u.Lastname = "Wicaksono"
+	u.Address = "Jakarta"
+
+	fmt.Println(u)
+}
+
+func (u User) SetProfile(fname string, lname string, addname string) {
+	u.Firstname = fname
+	u.Lastname = lname
+	u.Address = addname
+
+	fmt.Println(u)
+}
