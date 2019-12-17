19a744cde2693fbe2ebe63ca19b77e5f1a32f566
Agung Wicaksono <agungw@ecampus.ut.ac.id>
2019-12-17 11:16:53 +0700

    Add Interface blog.go

---
+++ Blog/blog.go
@@ -0,0 +1,28 @@
+package blog
+
+import (
+	"fmt"
+)
+
+type BlogInterface interface {
+	GetBlog()
+	SetBlog(string, string, string)
+}
+
+type Blog struct {
+	Title       string
+	Descritpion string
+	Author      string
+}
+
+func New(title string, desc string, author string) BlogInterface {
+	return Blog{title, desc, author}
+}
+
+func (b Blog) GetBlog() {
+	fmt.Println(b)
+}
+
+func (b Blog) SetBlog(title string, desc string, author string) {
+
+}
